823e16ef49a5b0d08380106f9039f2ff51065637
My Space <My Space@DESKTOP-G7E40EF>
2021-06-10 17:48:11 +1000

    Добавьте файлы проекта.

---
+++ Checkers/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Checkers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool moveLeft, moveRight;
+
+        int playerSpeed = 10;
+
+        Rect playerHitBox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = Environment.CurrentDirectory + "/Images/Donut.png";
+            Canvas.Background = new ImageBrush(new BitmapImage(new Uri(path)));
+
+            Canvas.Focus();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+            ImageBrush playerImage = new ImageBrush();
+            playerImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/RAcer.png"));
+            player.Fill = playerImage;
+
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            playerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+           
+            if (moveLeft == true && Canvas.GetLeft(player) > 0)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed);
+            }
+            if (moveRight == true && Canvas.GetLeft(player) + 80 < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = true;
+            }
+        }
+
+        private void OnKeyUP(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = false;
+            }
+        }
+    }
+}
